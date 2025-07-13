df75f6c746bdd98be78483673f230c1b482b1160
janesh-web3 <janeshtimilsena963@gmail.com>
2025-07-13 18:17:32 +0545

    fix cors and sever

--- admin/src/server.ts
+++ admin/src/server.ts
@@ -1,14 +1,27 @@
-export const server = "http://localhost:8080/api";
-// export const server = "https://server.om-shreenagar-dental-clinic.com/api";
-// export const server = "https://server3.crownagi.com/api"; 
 
+// local 
+// export const server = "http://localhost:8080/api";
+// export const  dentalName = "Crown Dental Clinic";
+// export const socket = "http://localhost:8080";
+// export const dentalAddress = "Taranagar, Dhangadhi, Nepal";
+
+// shree nagar dental
+// export const server = "https://server.om-shreenagar-dental-clinic.com/api";
+// export const socket = "https://server.om-shreenagar-dental-clinic.com";
+// export const  dentalName = "Om Shree Nagar Dental Clinic";
+// export const dentalAddress = "Parkmod, Dhangadhi, Nepal";
+
+// crown dental clinic
+// export const server = "https://server3.crownagi.com/api"; 
+// export const socket = "https://server3.crownagi.com";
+// export const dentalAddress = "Taranagar, Dhangadhi, Nepal";
 // export const  dentalName = "Crown Dental Clinic";
 
-export const  dentalName = "Om Shree Nagar Dental Clinic";
 
-export const dentalAddress = "Taranagar, Dhangadhi, Nepal";
+// muskan dental
+export const server = "https://muskanserver.crownagi.com/api";
+export const socket = "https://muskanserver.crownagi.com";
+export const  dentalName = "Muskan Dental Clinic";
+export const dentalAddress = "Parkmod, Dhangadhi, Nepal";
 
-export const socket = "http://localhost:8080";
-// export const socket = "https://server.om-shreenagar-dental-clinic.com";
-// export const socket = "https://server3.crownagi.com";
 
